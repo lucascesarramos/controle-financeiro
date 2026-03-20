8b1ea09da39f72f5cea3774d995b7f8426df8e92
lucascesarramos <lucascesarramos@gmail.com>
2026-03-20 16:06:23 -0300

    calendario

--- pages/3_Calendário.py
+++ pages/3_Calendário.py
@@ -68,7 +68,7 @@ meses_pt = {
 
 df, ultima_atualizacao = load_data()
 
-df = global_filters(df, default_ultimo_mes=False)
+df = global_filters(df, default_ultimo_mes=True)
 
 page_header("Calendário de Transações", ultima_atualizacao)
 
